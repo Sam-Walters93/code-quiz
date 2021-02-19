4da7833457e5e9e7da4f2e8a6b45be01f8d4d4a1
Sam <stwalters314@gmail.com>
2021-02-19 15:08:56 -0600

    began transcribing question objs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,51 @@
 
-//Start Button code 
+var questions = 
+[
+{question: "Inside which HTML element do we put the JavaScript?",
+options: ['a. <javascript>','b. <scripting>','c. <script>', 'd. <js>'],
+answer: 'a'}, 
+{question: "What is the correct JavaScript syntax to change the content of the HTML element below\? <br><p id= demo>This is a demonstration.</p>",
+options: ['a. #demo.innerHTML = "Hello World"','b. document.getElementByName("p").innerHTML', 'c. document.getElementById("demo").innerHTML = "Hello World"', 'd. document.getElement("p").innerHTML = "Hello World"'],
+answer: 'c'}, 
+{question: "Where is the correct place to insert a JavaScript?",
+options: ['a. Both the <head> and <body> sections are correct','b. the <head> section','c. the <body> section'],
+answer: 'a'}, 
+{question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+options: ['a. <script name="xxx.js">','b. <script href="xxx.js">','c. <script url(xxx.js)>', 'd. <script src="xxx.js">'],
+answer: 'b'},
+{question: "The external JavaScript file must contain the <script> tag.",
+options: ['a. True','b. False'],
+answer: 'b'}, 
+{question: "How do you write 'Hello World' in an alert box?",
+options: ['a. alertBox("Hello World");','b. alert("Hello World");','c. msgBox("Hello World");', 'd. msg("Hello World");'],
+answer: 'b'},
+{question: "How do you create a function in JavaScript?",
+options: ['a. function:myFunction()','b. function myFunction()','c. function = myFunction', 'd. var function = ()'],
+answer: 'b'}, {question: "How do you call a function named 'myFunction'?",
+options: ['a. call function myFunction()','b. myFunction()','c. call myFunction()'],
+answer: 'b'}, {question: "How do you write an IF statement in JavaScript?",
+options: ['a. if i = 5','b. if i = 5 then','c. if i === 5', 'd. if (i === 5)'],
+answer: 'd'}, {question: "How do you write an IF statement if 'i' is NOT equal to 5?",
+options: ['a. if (i != 5)','b. if i =! 5 then','c. if (i <> 5)', 'd. if i <> 5'],
+answer: 'a'}, {question: "How does a WHILE loop start?",
+options: ['a. while (i <= 10)','b. while i = 10','c. while (i <=10; i++)'],
+answer: 'c'}, {question: "How does a FOR loop start?",
+options: ['a. for (i = 0; i <= 5)','b. for i = [1, 5]','c. for (i = 0; i <= 5; i++)', 'd. for (i <=5; i++)'],
+answer: 'c'}, {question: "How can you add a comment in a JavaScript?",
+options: ['a. "this is a comment','b. //this is a comment','c. <!--this is a comment-->', 'd. /* this is a comment*/'],
+answer: 'b'},
+    
+]
+
+var  frame = 
+{question: "How can you add a comment in a JavaScript?",
+options: ['a. "this is a comment','b. //this is a comment','c. <!--this is a comment-->', 'd. /* this is a comment*/'],
+answer: 'b'}, 
+
+
+ 
+
+
+
+    
 
